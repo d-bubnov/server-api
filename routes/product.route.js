05b815c1f034837dcf1b4b15cf7a8488ea68e17c
Denis Bubnov <denisbubnow@mail.ru>
2019-11-05 22:41:53 +0300

    Refactoring of the API methods

--- routes/product.route.js
+++ routes/product.route.js
@@ -6,62 +6,136 @@ const productRoutes = express.Router();
 let Product = require('../models/Product');
 
 // Defined store route
-productRoutes.route('/add').post(function (req, res) {
-  let product = new Product(req.body);
+productRoutes.route('/add').post(function (request, response) {
+  let product = new Product(request.body);
   product.save()
     .then(product => {
-      res.status(200).json({'Product': 'Product has been added successfully'});
+      response
+        .status(200)
+        .json({
+          'Message': 'Product has been added successfully',
+          'Success': true,
+          'Data': product._id,
+        });
     })
-    .catch(err => {
-    res.status(400).send("unable to save to database");
+    .catch(error => {
+      response
+        .status(400)
+        .json({
+          'Message': 'Unable to save product to the database',
+          'Success': false,
+          'Data': error,
+        });
     });
 });
 
 // Defined get data(index or listing) route
-productRoutes.route('/').get(function (req, res) {
-  Product.find(function (err, products){
-    if(err){
-      console.log(err);
-    }
-    else {
-      res.json(products);
+productRoutes.route('/').get(function (request, response) {
+  Product.find(function (error, products) {
+    if (error) {
+      response
+        .status(404)
+        .json({
+          'Message': 'Products not found',
+          'Success': false,
+          'Data': error,
+        });
+    } else {
+      response
+        .status(200)
+        .json({
+          'Message': 'Products was found',
+          'Success': true,
+          'Data': products,
+        });
     }
   });
 });
 
 // Defined edit route
-productRoutes.route('/edit/:id').get(function (req, res) {
-  let id = req.params.id;
-  Product.findById(id, function (err, product){
-      res.json(product);
+productRoutes.route('/edit/:id').get(function (request, response) {
+  let id = request.params.id;
+  Product.findById(id, function (error, product) {
+    if (error) {
+      response
+        .status(404)
+        .json({
+          'Message': 'Product not found',
+          'Success': false,
+          'Data': error,
+        });
+    } else {
+      response
+        .status(200)
+        .json({
+          'Message': 'Product was found',
+          'Success': true,
+          'Data': product,
+        });
+    }
   });
 });
 
 //  Defined update route
-productRoutes.route('/update/:id').post(function (req, res) {
-  Product.findById(req.params.id, function(err, product) {
+productRoutes.route('/update/:id').post(function (request, response) {
+  Product.findById(request.params.id, function(error, product) {
     if (!product)
-      res.status(404).send("Record not found");
+      response
+        .status(404)
+        .json({
+          'Message': 'Product not found',
+          'Success': false,
+          'Data': null,
+        });
     else {
-      product.ProductName = req.body.ProductName;
-      product.ProductDescription = req.body.ProductDescription;
-      product.ProductPrice = req.body.ProductPrice;
+      product.ProductName = request.body.ProductName;
+      product.ProductDescription = request.body.ProductDescription;
+      product.ProductPrice = request.body.ProductPrice;
 
-      product.save().then(product => {
-          res.json('Update complete');
-      })
-      .catch(err => {
-            res.status(400).send("unable to update the database");
-      });
+      product
+        .save()
+        .then(product => {
+          response
+            .status(200)
+            .json({
+              'Message': 'Update completed',
+              'Success': true,
+              'Data': null,
+            });
+        })
+        .catch(error => {
+          response
+            .status(400)
+            .json({
+              'Message': 'Unable to update product in the database',
+              'Success': false,
+              'Data': error,
+            });
+        });
     }
   });
 });
 
 // Defined delete | remove | destroy route
-productRoutes.route('/delete/:id').get(function (req, res) {
-    Product.findByIdAndRemove({_id: req.params.id}, function(err, product){
-        if(err) res.json(err);
-        else res.json('Successfully removed');
+productRoutes.route('/delete/:id').get(function (request, response) {
+    Product.findByIdAndRemove({ _id: request.params.id }, function(error, product) {
+      if (error) {
+        response
+          .status(400)
+          .json({
+            'Message': 'Unable to delete product in the database',
+            'Success': false,
+            'Data': error,
+          });
+      } else {
+        response
+          .status(200)
+          .json({
+            'Message': 'Successfully removed',
+            'Success': true,
+            'Data': null,
+          });
+      }
     });
 });
 
